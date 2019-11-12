aa520e5f43cfcfb2174f7c308b02de0c9b9980f8
shangxinbo <shangxb@startimes.com.cn>
2019-11-12 10:28:56 +0800

    解决图片问题

--- fe/vue.config.js
+++ fe/vue.config.js
@@ -3,43 +3,46 @@ const fs = require("fs");
 const glob = require("glob");
 
 function resolve(dir) {
-    return path.join(__dirname, dir);
+  return path.join(__dirname, dir);
 }
 
 const pages = {};
 let entries;
 try {
-    entries = glob("src/pages/**/main.js", { sync: true });
+  entries = glob("src/pages/**/main.js", { sync: true });
 } catch (err) {
-    entries = [];
-    throw err;
+  entries = [];
+  throw err;
 }
 
 entries.forEach(file => {
-    // eslint-disable-next-line no-console
-    const pageName = file.replace("src/pages/", "").replace("/main.js", "");
-    let pageHtml = file.replace("main.js", "index.html");
-    if (!fs.existsSync(pageHtml)) {
-        pageHtml = "public/index.html";
-    }
-    pages[pageName] = {
-        entry: file,
-        template: pageHtml,
-        filename: `${pageName}.html`
-    };
+  // eslint-disable-next-line no-console
+  const pageName = file.replace("src/pages/", "").replace("/main.js", "");
+  let pageHtml = file.replace("main.js", "index.html");
+  if (!fs.existsSync(pageHtml)) {
+    pageHtml = "public/index.html";
+  }
+  pages[pageName] = {
+    entry: file,
+    template: pageHtml,
+    filename: `${pageName}.html`
+  };
 });
 
 module.exports = {
-    configureWebpack: {
-        resolve: {
-            alias: {
-                "@": resolve("src")
-            }
-        }
-    },
-    assetsDir: "assets",
-    pages: pages,
-    devServer: {
-        port: 1024
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "@": resolve("src")
+      }
     }
+  },
+  css: {
+    extract: false
+  },
+  assetsDir: "assets",
+  pages: pages,
+  devServer: {
+    port: 1024
+  }
 };
