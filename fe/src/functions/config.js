141d508db2a5da53f2ebfb9fb23b02daaf8ce575
Tiwen007 <18811716915@163.com>
2019-11-16 10:14:32 +0800

    修改配置

--- fe/src/functions/config.js
+++ fe/src/functions/config.js
@@ -15,7 +15,7 @@ if (location.hostname.indexOf('localhost') >= 0 || location.hostname.indexOf('10
         gaIosKey: 'UA-55969660-8',
         countlyAppKey: 'cb95b4e9125098b99a7df2fbda4554eda982918b',
         countlyServer: 'http://10.0.243.111:8080',
-        apiUrl: 'http://qa.upms.startimestv.com'
+        apiUrl: 'http://dev.upms.startimestv.com'
     }
 } else if (location.hostname.indexOf('test') >= 0) {
     env = {
