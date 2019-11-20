131206e52fda8d29469f8ec54d7c5b8818f10f77
shangxinbo <shangxb@startimes.com.cn>
2019-11-20 22:58:13 +0800

    修复如果用户id是空的情况不像后台请求接口

--- fe/src/functions/app.js
+++ fe/src/functions/app.js
@@ -293,7 +293,11 @@ export const getQueryVariable = function(query, key) {
   for (let i = 0; i < vars.length; i++) {
     const pair = vars[i].split('=')
     if (decodeURIComponent(pair[0]) == key) {
-      return decodeURIComponent(pair[1])
+      if (pair[1]) {
+        return decodeURIComponent(pair[1])
+      } else {
+        return null
+      }
     }
   }
 }
