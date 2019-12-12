01ad52ab33bd37a3409a21b63941f45df9a675d0
Tiwen007 <18811716915@163.com>
2019-12-12 09:59:21 +0800

    弹幕优化

--- fe/src/pages/activity/BSSVote2/App.vue
+++ fe/src/pages/activity/BSSVote2/App.vue
@@ -431,7 +431,6 @@ export default {
           this.$nextTick(() => {
             this.initPage()
           })
-          this.barrageIsShow = true
           this.getCommentList()
         }
       }
@@ -545,7 +544,8 @@ export default {
             this.last_id = res.data.data[res.data.data.length - 1].id
             this.commentList = res.data.data
             this.canClickTab1 = true
-            this.barrageIndex = 0
+            this.addToList(this.commentList[0])
+            this.barrageIndex = 1
             this.t = setInterval(() => {
               this.addToList(this.commentList[this.barrageIndex])
               this.barrageIndex++
--- fe/src/pages/activity/BSSVote3/App.vue
+++ fe/src/pages/activity/BSSVote3/App.vue
@@ -429,7 +429,6 @@ export default {
           this.$nextTick(() => {
             this.initPage()
           })
-          this.barrageIsShow = true
           this.getCommentList()
         }
       }
@@ -542,7 +541,8 @@ export default {
             this.last_id = res.data.data[res.data.data.length - 1].id
             this.commentList = res.data.data
             this.canClickTab1 = true
-            this.barrageIndex = 0
+            this.addToList(this.commentList[0])
+            this.barrageIndex = 1
             this.t = setInterval(() => {
               this.addToList(this.commentList[this.barrageIndex])
               this.barrageIndex++
